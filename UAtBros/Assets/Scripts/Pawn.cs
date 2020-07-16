f3a18c0249f01f9bdba2d7dea095123a7b09aca1
Jacquelynne <jacheima@uat.edu>
2020-07-15 17:00:00 -0700

    Working with amanda on how to add animations

---
+++ UAtBros/Assets/Scripts/Pawn.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Pawn : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    public LayerMask groundLayer;
+
+    private Rigidbody2D body2D;
+    private Animator animator;
+    private BoxCollider2D boxCollider2D;
+
+
+    private void Start()
+    {
+        body2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    public void Move(float direction)
+    {
+        if(direction != 0f)
+        {
+            animator.SetBool("isRunning", true);
+
+            if(direction < 0)
+            {
+                direction = -1;
+            }
+            else if(direction > 0)
+            {
+                direction = 1;
+            }
+
+            Vector3 myScale = transform.localScale;
+            myScale.x = direction;
+            transform.localScale = myScale;
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        Vector3 myPos = transform.position;
+        myPos += transform.right * direction * moveSpeed * Time.deltaTime;
+        transform.position = myPos;
+    }
+
+    public void Jump()
+    {
+        body2D.velocity = Vector2.up * jumpForce;
+        animator.SetBool("isJumping", true);
+
+    }
+
+    public bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, groundLayer);
+
+        if(hit.collider != null)
+        {
+            Debug.Log("I AM GROUNDED!!!! NOOOOO!!!!");
+            animator.SetBool("isJumping", false);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+}
---
+++ UAtBros/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [HideInInspector]public Pawn pawn;
+
+    private void Start()
+    {
+        pawn = GetComponent<Pawn>();
+    }
+
+    private void Update()
+    {
+        //our direction to move
+        float direction = 0;
+
+        //if the player moves left
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction = -1;
+        }
+        
+        //if the player moves right
+        if(Input.GetKey(KeyCode.D))
+        {
+            direction = 1;
+        }
+
+        //if the player presses jump
+        if(pawn.IsGrounded() && Input.GetKeyDown(KeyCode.W ))
+        {
+            pawn.Jump();
+
+        }
+
+        pawn.Move(direction);
+    }
+}
